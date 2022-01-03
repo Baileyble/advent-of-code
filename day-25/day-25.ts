e2c5f6d84b7bd51d8c655f989d3f1808fbc62767
BaileyLe <ble@messerfinancialc.com>
2022-01-03 17:20:32 -0500

    fsdfsd

--- day-25/day-25.ts
+++ day-25/day-25.ts
@@ -25,37 +25,104 @@ function parseInput(input: string[]): string[][] {
 function testDay25() {
     const data = parseInput(day25Input);
 
+    let prev = data;
+    let curr = theMovement(data);
+    let runs = 0
 
+    while (prev !== curr) {
+      prev = curr;
+      curr = theMovement(prev);
+      console.table(curr)
+      runs++
+    }
+
+    console.log(runs);
+    return runs
 }
 
-function theMovement(data: string[][], direction: 'east' | 'south') {
+function theMovement(data: string[][]): string[][] {
 
-    data.reduce((canMoveCoord : Array<{x: number, y: number}>, row, index) => {
+    let cucumberMap = data;
+
+    const eastMovingCoord = getEastMovingCoord(data);
+
+    const newEastPositions = eastMovingCoord.map(coord => {
+        if (coord.x < data[0].length) {
+            coord.x = coord.x++
+        } else {
+            coord.x = 0
+        }
+
+        return coord
+    })
+
+    eastMovingCoord.forEach(coord => {
+        cucumberMap[coord.y][coord.x] = '.';
+
+    })
+
+    newEastPositions.forEach(coord => {
+        cucumberMap[coord.y][coord.x] = '>';
+    })
+
+
+    const southMovingCoord = getSouthMovingCoord(data);
+
+    const newSouthPositions = southMovingCoord.map(coord => {
+        if (coord.y < data.length) {
+            coord.y = coord.y++
+        } else {
+            coord.y = 0
+        }
+
+        return coord
+    })
+
+    southMovingCoord.forEach(coord => {
+        cucumberMap[coord.y][coord.x] = '.';
+    })
+
+    newSouthPositions.forEach(coord => {
+        cucumberMap[coord.y][coord.x] = 'v';
+    })
+
+    return cucumberMap
+}
+
+function getEastMovingCoord(data: string[][]): Array<{x: number, y: number}> {
+    return data.reduce((canMoveCoord : Array<{x: number, y: number}>, row, index) => {
         let xCoord: number[]
-        if (direction === 'east') {
-            xCoord = row.reduce((canMoveIndex: number[], item, i) => {
-                 if (item === '>' && ((!!row[i + 1] && row[i + 1] === '.') || !row[i + 1] && row[0] === '.')) {
-                     canMoveIndex.push(i);
-                 }
-                 return canMoveIndex
-            }, [])
-        }
+        xCoord = row.reduce((canMoveIndex: number[], item, i) => {
+            if (item === '>' && ((!!row[i + 1] && row[i + 1] === '.') || (!row[i + 1] && row[0] === '.'))) {
+                canMoveIndex.push(i);
+            }
+            return canMoveIndex
+        }, [])
 
-        if (direction === 'south') {
-
-            
-
-        }
-
-
-
-        // @ts-ignore
         const coordArr = xCoord.map((x) => {
             return {x: x, y: index}
         })
 
-        return  canMoveCoord.concat(...coordArr)
+        return canMoveCoord.concat(...coordArr)
     }, [])
-
-
 }
+
+function getSouthMovingCoord(data: string[][]): Array<{x: number, y: number}> {
+    return data[0].reduce((canMoveCoord: Array<{x: number, y: number}>, colHead, index) => {
+        const yCoord: number[] = []
+        for (let i: number = 0; i < data.length; i++) {
+            const item = data[i][index]
+            if (item === 'v' && ((!!data[i + 1] && data[i + 1][index] === '.') || (!data[i + 1] && data[0][index] === '.'))) {
+                yCoord.push(i);
+            }
+        }
+
+        const coordArr: Array<{x: number, y: number}> = yCoord.map(y => {
+            return {x: index, y: y}
+        });
+
+        return canMoveCoord.concat(...coordArr);
+    }, [])
+}
+
+testDay25();
