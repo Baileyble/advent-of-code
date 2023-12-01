4da30977ad4cccfb783b5d13a9e74619dc9d8ef3
BaileyLe <ble@messerfinancial.com>
2023-12-01 11:13:22 -0500

    removed filter

--- 2023/day-1/day-1.ts
+++ 2023/day-1/day-1.ts
@@ -9,7 +9,7 @@ function dayOne(input: string[]) {
 const numberWords = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
 function dayOnePartTwo(input: string[]) {
-    return input.filter(v => !!v).map(v => v.split('').reduce((acc, cv) => !!numberWords.find(num => acc.includes(num)) ? acc.replace(numberWords.find(num => acc.includes(num))!, numberWords.find(num => acc.includes(num))!.slice(0, 1) + (numberWords.findIndex(num => acc.includes(num)) + 1).toString() + numberWords.find(num => acc.includes(num))!.slice(-1)) : acc, v).replace(/\D/g,'')).filter(v => !!v).map((v) => v[0].toString() + v.slice(-1).toString()).reduce((acc, cv) => acc + +cv, 0);
+    return input.map(v => v.split('').reduce((acc, cv) => !!numberWords.find(num => acc.includes(num)) ? acc.replace(numberWords.find(num => acc.includes(num))!, numberWords.find(num => acc.includes(num))!.slice(0, 1) + (numberWords.findIndex(num => acc.includes(num)) + 1).toString() + numberWords.find(num => acc.includes(num))!.slice(-1)) : acc, v).replace(/\D/g,'')).filter(v => !!v).map((v) => v[0].toString() + v.slice(-1).toString()).reduce((acc, cv) => acc + +cv, 0);
 }
 
 console.log(dayOne(day1Input));
